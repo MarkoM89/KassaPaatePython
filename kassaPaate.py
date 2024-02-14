767c77babd10de1fa3bc859c5e2dd2cf6cf236eb
Marko Mustonen <marko.mustonen3@metropolia.fi>
2024-02-14 19:17:18 +0200

    Poistettu kassapäätteestä varastonhallinnan toiminnot

--- kassaPaate.py
+++ kassaPaate.py
@@ -55,23 +55,12 @@ print("\n\n\n")
 '''
 Päätoiminto 1: Osta tuote 
 
-Päätoiminto 2: Maksukorttin hallinta
-		Toiminto 1: Luo Maksukortti
-		Toiminto 2: Lataa rahaa
-		Toiminto 3: Poista maksukortti
-		Toiminto 4: Tulosta korttien tiedot
-
-Päätoiminto 3: Tuotehallinta
-		Toiminto 1: Lisää tuote
-		Toiminto 2: Poista tuote
-		Toiminto 3: Tulosta tuotteet
-		
-Päätoiminto 4: Poistu ohjelmasta
+Päätoiminto 2: Poistu ohjelmasta
 '''
 
-while toiminto != 4:
-    toiminto = int(input("\nPäätoiminto 1: Ostotapahtuma\nPäätoiminto 2: Varaston hallinta\nPäätoiminto 3: Tuotehallinta\n"
-        			+ "Päätoiminto 4: Poistu ohjelmasta\nPäätoiminto 5: Tulosta korttien tiedot\n"))
+while toiminto != 2:
+    toiminto = int(input("\nPäätoiminto 1: Ostotapahtuma\n"
+        			+ "Päätoiminto 2: Poistu ohjelmasta\nPäätoiminto 5: Tulosta korttien tiedot\n"))
 
     if toiminto == 1:
         print("Päätoiminto 1: Ostotapahtuma")
@@ -100,39 +89,8 @@ while toiminto != 4:
                 kuitit[-1].tulostaKuitti()
                 ostetutTuotteet.clear()
                 loppusumma = 0
-
+        
     elif toiminto == 2:
-        print("Päätoiminto 2: Varaston hallinta")
-
-    elif toiminto == 3:
-        while toiminto != 4:
-
-            print("\nPäätoiminto 3: Tuotehallinta")
-				
-            toiminto = int(input("\nToiminto 1: Lisää tuote\nToiminto 2: Poista tuote\nToiminto 3: Tulosta tuotteet\n"
-				    		+ "Toiminto 4: Poistu tuotehallinnasta\n"))
-        
-            if toiminto == 1:
-                nimi = (input("Tuotenimi: "))
-                hinta = float(input("Tuotteen yksikköhinta: "))
-                tuotteet.append(Tuote(nimi, hinta))
-            
-            elif toiminto == 2:
-                nimi = input("Tuotenimi? Tyhjä kenttä ei poista mitään: ")
-                for tuote in tuotteet:
-                    if tuote.haeNimi() == nimi:
-                        tuotteet.remove(tuote)
-                    else:
-                        #Tuotetta ei löytynyt tulostuu, joka kierroksella, virhe
-                        print("Tuotetta ei löytynyt")
-
-            elif toiminto == 3:
-                print("Tuotelista")
-                for tuote in tuotteet:
-                    tuote.tulostaTuote()
-        toiminto = 0
-        
-    elif toiminto == 4:
         print("Ohjelma sulkeutuu")
 
     elif toiminto == 5:
